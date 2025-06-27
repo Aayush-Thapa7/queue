ffb8a7d9a0e05fe5f3d9d372937b04e85cccc2f8
Ayush Thapa <aayuthapa2242@gmail.com>
2025-06-27 23:32:30 +0545

    commit

---
+++ queue2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+    Node(int n){
+        data=n;
+        next= nullptr;
+    }
+};
+
+class queue{
+    Node* head;
+    Node* tail;
+})
